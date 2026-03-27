473c7c14c98b7f0a3287d0631bda59eccd8aa520
I-Sadeque <ilham_sadeque@outlook.com>
2026-03-27 14:58:40 +0000

    Completed task 3

---
+++ marine_experiments/queries/task_3.sql
@@ -0,0 +1,12 @@
+-- returns the a table of data that shows the average score for each species in each test
+SELECT 
+    et.type_name, 
+    sp.species_name, 
+    ROUND(AVG(e.score), 1) AS average_score
+FROM experiment e
+JOIN experiment_type et ON e.experiment_type_id = et.experiment_type_id
+JOIN subject s ON e.subject_id = s.subject_id
+JOIN species sp ON s.species_id = sp.species_id
+GROUP BY et.type_name, sp.species_name
+HAVING AVG(e.score) > 5
+ORDER BY average_score DESC;
