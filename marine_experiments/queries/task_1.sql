b7e593b33e6557dc951b684b13b23680c7cff258
I-Sadeque <ilham_sadeque@outlook.com>
2026-03-27 14:34:37 +0000

    Task 1 completed

--- marine_experiments/queries/task_1.sql
+++ marine_experiments/queries/task_1.sql
@@ -1,1 +1,9 @@
-
+--returns a subjects data and there species
+SELECT 
+    s.subject_id, 
+    s.subject_name, 
+    sp.species_name, 
+    TO_CHAR(s.date_of_birth, 'YYYY-MM') AS date_of_birth
+FROM subject s
+JOIN species sp ON s.species_id = sp.species_id
+ORDER BY s.date_of_birth DESC;
